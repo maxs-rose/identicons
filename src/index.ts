92316f3f158c78eb3d606a4a60d8a4dcfb3b9964
Max Rose <max.rose366@gmail.com>
2022-10-02 14:35:55 +0100

    Write square svg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-export const log = () => {
-	console.log('h ei');
+import { createSquareSvg } from './svgWriter';
+
+export const generateIconString = (data: string, options?: Partial<{ size: number; width: number }>) => {
+	return createSquareSvg(
+		[
+			{ x: 0, y: 0, color: '#f8427e' },
+			{ x: 1, y: 0, color: '#f8427e' },
+			{ x: 2, y: 0, color: '#f8427e' },
+			{ x: 3, y: 0, color: '#f8427e' },
+			{ x: 4, y: 0, color: '#f8427e' },
+			{ x: 0, y: 4, color: '#f8427e' },
+			{ x: 1, y: 4, color: '#bbb529' },
+			{ x: 2, y: 4, color: '#f8427e' },
+			{ x: 3, y: 4, color: '#f8427e' },
+			{ x: 4, y: 4, color: '#f8427e' },
+		],
+		{ size: 5, imageSize: 128, ...options }
+	);
+};
+
+export const generateIconElement = (data: string, options?: {}): SVGElement => {
+	const element = document.createElement('container');
+	element.innerHTML = generateIconString(data, options);
+
+	return element.firstChild as SVGElement;
 };
---
+++ src/svgWriter.ts
@@ -0,0 +1,51 @@
+export const createSquareSvg = (
+	items: { x: number; y: number; color: string }[],
+	options: { size: number; imageSize: number }
+) => {
+	allBoxPositionsValid(items, options.size);
+
+	const svg = [];
+	svg.push(
+		`<svg width="${options.imageSize}" height="${options.imageSize}" viewBox="0 0 ${options.imageSize} ${options.imageSize}" preserveAspectRatio="xMinYMin" xmlns="http://www.w3.org/2000/svg"> `
+	);
+
+	const boxSize = Math.floor(options.imageSize / (options.size + 1));
+	const positions = boxPositionCalculator(options.size, options.imageSize);
+
+	items.forEach((i) => {
+		const index = positions.project(i.x, i.y);
+		svg.push(
+			`<rect x="${positions.x(index)}" y="${positions.y(index)}" width="${boxSize}" height="${boxSize}" fill="${
+				i.color
+			}" />`
+		);
+	});
+
+	svg.push('</svg>');
+
+	return svg.join('\n');
+};
+
+const allBoxPositionsValid = (positions: { x: number; y: number }[], size: number) => {
+	let allValid = true;
+	positions.forEach((item) => {
+		if (item.x >= 0 && item.x < size && item.y >= 0 && item.y < size) return;
+
+		console.error('Element did not pass validation', item);
+
+		allValid = false;
+	});
+
+	if (!allValid) throw new Error('Invalid box positions for icon found!');
+};
+
+const boxPositionCalculator = (size: number, imageSize: number) => {
+	const boxSize = Math.floor(imageSize / (size + 1));
+	const marginWidth = Math.floor(boxSize / 2 + (imageSize % (size + 1)) / 2);
+
+	return {
+		project: (x: number, y: number) => x + y * size,
+		x: (i: number) => marginWidth + (i % size) * boxSize,
+		y: (i: number) => marginWidth + (Math.floor(i / size) % size) * boxSize,
+	};
+};
